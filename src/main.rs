6fb36598695a4d8bb4d0a5db65b01d3b250a7ae8
trillian <trillian.git@r9.pm>
2024-11-28 02:29:53 +0200

    sel's initial

---
+++ src/main.rs
@@ -0,0 +1,169 @@
+pub const WIDTH: usize = 5;
+pub const HEIGHT: usize = 5;
+
+#[derive(Debug,Clone)]
+pub struct Board {
+    // 0 - empty, 1/2 - player 1/2 upright, -1/-2 - player 1/2 flipped
+    cells: [i8;WIDTH*HEIGHT]
+}
+impl Board {
+    pub fn new() -> Self {
+        Self { cells: [0; WIDTH*HEIGHT] }
+    }
+    pub fn score(&self) -> i32 {
+        let mut score = 0;
+        for idx in 0..self.cells.len() {
+            let x = idx%WIDTH;
+            let y = idx/WIDTH;
+            let piece = self.cells[idx];
+            let sum = self.neighbor_count(idx,x,y);
+            if sum >= 3 {
+                if piece == -1 { score += 1; }
+                if piece == -2 { score -= 1; }
+            }
+        }
+        score
+    }
+    pub fn neighbor_count(&self, idx: usize, x: usize, y: usize) -> i32 {
+        let piece = self.cells[idx].abs();
+        let mut sum = 0;
+        let w = WIDTH;
+        let h = HEIGHT;
+        sum += (x > 0   && self.cells[idx-1].abs() == piece) as i32;
+        sum += (x < w-1 && self.cells[idx+1].abs() == piece) as i32;
+        sum += (y > 0   && self.cells[idx-w].abs() == piece) as i32;
+        sum += (y < h-1 && self.cells[idx+w].abs() == piece) as i32;
+        sum
+    }
+    pub fn update(&mut self, idx: usize, x: usize, y: usize) {
+        let piece = self.cells[idx];
+        if piece <= 0 { return; }
+        let sum = self.neighbor_count(idx,x,y);
+        if sum >= 3 {
+            self.cells[idx] *= -1;
+        }
+    }
+    pub fn propagate(&mut self, idx: usize) {
+        let x = idx%WIDTH;
+        let y = idx/WIDTH;
+        let w = WIDTH;
+        let h = HEIGHT;
+        self.update(idx,x,y);
+        if x > 0   { self.update(idx-1,x-1,y); }
+        if x < w-1 { self.update(idx+1,x+1,y); }
+        if y > 0   { self.update(idx-w,x,y-1); }
+        if y < h-1 { self.update(idx+w,x,y+1); }
+    }
+    pub fn moves(&self, player: i8) -> Vec<Board> {
+        let mut out = vec![];
+        for c in 0..self.cells.len() {
+            if self.cells[c] == 0 {
+                let mut b = self.clone();
+                b.cells[c] = player+1;
+                b.propagate(c);
+                out.push(b);
+            }
+        }
+        let mut swaps = vec![];
+        for i in 0..self.cells.len() {
+            if self.cells[i] > 0 {
+                swaps.push(i);
+            }
+        }
+        for &i in &swaps {
+            for &j in &swaps {
+                if i >= j { continue; }
+                let mut b = self.clone();
+                b.cells.swap(i,j);
+                b.cells[i] *= -1;
+                b.cells[j] *= -1;
+                b.propagate(i);
+                b.propagate(j);
+                out.push(b);
+            }
+        }
+        out
+    }
+    pub fn hash(&self) -> u128 {
+        self.cells.iter().fold(0u128,|a,e| {
+            (a << 3) | ((e+2) as u128)
+        })
+    }
+}
+impl std::fmt::Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        writeln!(f, "===")?;
+        for (i,c) in self.cells.iter().enumerate() {
+            if i != 0 && i % WIDTH == 0 { writeln!(f)?; }
+            write!(f, "{}", b"OX.xo"[(c+2) as usize] as char)?;
+        }
+        writeln!(f)
+    }
+}
+
+#[derive(Default)]
+pub struct Solver {
+    cache: std::collections::HashMap<u128, ((i32,f32),Option<Board>)>,
+}
+impl Solver {
+    pub fn minimax(&mut self, b: &Board, player: i8, depth: i32) -> ((i32,f32),Option<Board>) {
+        if let Some((i,b)) = self.cache.get(&b.hash()) { return (*i,b.clone()); }
+        let f = b.moves(player);
+        let mut child_avg = 0.0;
+        let mut best_so_far = (0,0.0);
+        let mut best_board = None;
+        let l = f.len();
+        if depth >= 5 {
+            return ((b.score(), b.score() as f32), None);
+        }
+        for (idx,i) in f.into_iter().enumerate() {
+            if depth <= 1 {
+                //println!("depth={} i={}/{} best={:?}", depth,idx,l, best_so_far);
+            }
+            let (s,b) = self.minimax(&i,player^1,depth+1);
+            child_avg += s.1;
+            let better = if player == 0 {
+                s > best_so_far
+            } else {
+                s < best_so_far
+            };
+            if best_board.is_none() || better {
+                best_so_far = s;
+                best_board = Some(i);
+            }
+            //if (player == 0 && best_so_far > 0) || (player == 1 && best_so_far < 0) {
+                //break; 
+            //}
+        }
+        child_avg /= l as f32;
+        best_so_far.1 = child_avg;
+        if best_board.is_none() { best_so_far.0 = b.score(); best_so_far.1 = best_so_far.0 as f32; }
+        self.cache.insert(b.hash(), (best_so_far, best_board.clone()));
+        (best_so_far,best_board)
+    }
+}
+
+
+fn main() {
+    use rand::Rng;
+    use rand::seq::SliceRandom;
+    let mut rng = rand::thread_rng();
+    let mut b = Board::new();
+    let mut solver = Solver::default();
+    let mut turn = 0;
+    loop {
+        solver.cache.clear();
+        if true && turn&1 == 1 {
+            let moves = b.moves(turn&1);
+            if moves.len() == 0 { return; }
+            b = moves.choose(&mut rng).unwrap().clone();
+        } else {
+            let (s,br) = solver.minimax(&b, turn&1, 0);
+            if br.is_none() { return; }
+            b = br.unwrap();
+        }
+        turn += 1;
+        println!("{}", b);
+        println!("Score {}", b.score());
+    }
+}
