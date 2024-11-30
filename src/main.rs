f2207d1da602ec9a488f1222be81a73f322a50cb
trillian <trillian.git@r9.pm>
2024-11-30 02:46:22 +0200

    nuke zobrist (finally)
    it had a good run

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,6 @@ pub const WIDTH: usize = 5;
 pub const HEIGHT: usize = 5;
 pub const NEIGHBORS: i32 = 3;
 
-static mut ZOBRIST: [u64; WIDTH*HEIGHT*4] = [0; WIDTH*HEIGHT*4];
-fn get_zobrist(ind: usize, el: u8) -> u64 {
-    unsafe { ZOBRIST[ind*4 + (el & 3) as usize] }
-}
-
 /// returns bitmasks of positions with 3 and 2 neighbors
 pub fn count_neighbors(board: u32) -> (u32, u32) {
     let a = (board >> 1) & 0b01111_01111_01111_01111_01111 & board; // has left neighbor
@@ -23,7 +18,7 @@ pub fn count_neighbors(board: u32) -> (u32, u32) {
     (has_3_neighs, has_2_neighs)
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Board {
     // bitboards: whether this cell is occupied at all
     nonempty: u32,
@@ -31,12 +26,10 @@ pub struct Board {
     player: u32,
     // if nonempty, is this piece flipped
     flipped: u32,
-    // precomputed hash
-    hash: u64,
 }
 impl Board {
     pub fn new() -> Self {
-        Self { nonempty: 0, player: 0, flipped: 0, hash: 0, }
+        Self { nonempty: 0, player: 0, flipped: 0, }
     }
     pub fn score_one_player(&self, player: bool) -> i32 {
         let board = if player { self.nonempty & self.player } else { self.nonempty & !self.player };
@@ -53,14 +46,6 @@ impl Board {
         let board = if player { self.nonempty & self.player } else { self.nonempty & !self.player };
         let to_flip = count_neighbors(board).0 & !self.flipped;
         self.flipped ^= to_flip;
-        if to_flip != 0 {
-            let mut tmp = to_flip;
-            while tmp != 0 {
-                let pos = tmp.trailing_zeros();
-                self.hash ^= get_zobrist(pos as usize, player as u8) ^ get_zobrist(pos as usize, 2 + player as u8);
-                tmp ^= 1<<pos;
-            }
-        }
     }
     pub fn moves(self, player: bool) -> impl Iterator<Item = Board> {
         gen move {
@@ -70,7 +55,6 @@ impl Board {
                     let mut b = self.clone();
                     b.nonempty |= 1<<c;
                     b.player |= (1<<c)*(player as u32);
-                    b.hash ^= get_zobrist(c, player as u8);
                     b.propagate(player);
                     yield b;
                 }
@@ -85,11 +69,8 @@ impl Board {
                     let i_player = (self.player >> i) & 1;
                     let j_player = (self.player >> j) & 1;
                     b.flipped |= 1<<i | 1<<j;
-                    if i_player != j_player { b.player ^= 1<<i | 1<<j; }
-                    let el1 = i_player as u8;
-                    let el2 = j_player as u8;
-                    b.hash ^= get_zobrist(i, el1) ^ get_zobrist(j, el1+2) ^ get_zobrist(j, el2) ^ get_zobrist(i, el2+2);
                     if i_player != j_player {
+                        b.player ^= 1<<i | 1<<j;
                         b.propagate(false); b.propagate(true);
                     }
                     yield b;
@@ -97,18 +78,8 @@ impl Board {
             }
         }.into_iter()
     }
-    pub fn hash(&self) -> u64 {
-        // this is slow, to be used for correctness verification only
-        let mut hash = 0u64;
-        for i in 0..25 {
-            if self.nonempty >> i & 1 != 0 {
-                hash ^= get_zobrist(i, ((self.flipped >> i & 1) << 1) as u8 | (self.player >> i & 1) as u8);
-            }
-        }
-        hash
-    }
 }
-type BoardHash = u64;
+type BoardHash = (Board, bool);
 
 impl std::fmt::Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -133,15 +104,12 @@ pub struct Solver {
     use_old_cache: bool,
 }
 
-const PLAYER_HASH: u64 = 0x1337;
 impl Solver {
     pub fn minimax(&mut self, b: &Board, player: bool, depth: i32, mut alpha: i32, beta: i32) -> (i32,Option<Board>) {
-        debug_assert!(b.hash() == b.hash);
-        //assert!(b.score() == b.score);
         if depth == 0 {
             return (b.score() * (1 - 2 * player as i32), None);
         }
-        let bhash = b.hash ^ PLAYER_HASH*(player as u64);
+        let bhash = (b.clone(), player);
         // if we've seen this state on this iteration...
         if let Some((i,b)) = self.cache.get(&bhash) { return (*i,b.clone()); }
         let mut best_so_far = 0;
@@ -179,23 +147,12 @@ impl Solver {
     }
 }
 
-fn init_zobrist() {
-    use rand::{RngCore, SeedableRng};
-    let mut rng = rand::rngs::StdRng::seed_from_u64(1337);
-    for i in 0..WIDTH*HEIGHT*4 {
-        unsafe {
-            ZOBRIST[i] = rng.next_u64();
-        }
-    }
-}
-
 fn main() {
     use rand::seq::SliceRandom;
     let mut rng = rand::thread_rng();
     let mut b = Board::new();
     let mut solver = Solver::default();
     let mut turn = 0;
-    init_zobrist();
     loop {
         if false && turn&1 == 1 {
             let moves: Vec<_> = b.moves(turn&1 == 1).collect();
